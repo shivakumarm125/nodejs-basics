5046985bdb0909d82a0d01d895ca1d58127c01a3
shivakumarm125 <shivakumarm.2501@gmail.com>
2025-01-02 23:08:17 +0000

    adding variable

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 require("./xyz.js");
 
+const {x , calculateSum} = require("./calculateSum.js");
+
+calculateSum(10, 20);
+
 console.log("this is main app");
+
+console.log(x);
---
+++ calculateSum.js
@@ -0,0 +1,7 @@
+function calculateSum(a, b) {
+  console.log(a + b);
+}
+
+var x = 10;
+
+module.exports = { x, calculateSum}; 
